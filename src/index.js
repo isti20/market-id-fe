07d87496cac4c8bf7afec67b8a4ea7a64a11e79f
iis istiqomah <iisistiqomah20@gmail.com>
2023-09-03 23:36:19 +0700

    feat: create page register and validasi form

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import routers from './routers';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "./assets/css/typograph.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/routers/index.js
+++ src/routers/index.js
@@ -6,13 +6,21 @@ import {
 
 // Import component, layout, page and auth
 import App from "../App";
+
+// Customer
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+// Negative page
 import Error from "../pages/Error";
 
 export default createBrowserRouter(
     createRoutesFromElements(
         <Route element={<App />}>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="*" element={<Error />} />
         </Route>
     )
