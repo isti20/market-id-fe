4df0d60ce0f469c559a0a8746f0e1a00fef231c2
iis istiqomah <iisistiqomah20@gmail.com>
2023-10-26 16:18:19 +0700

    add validation form & handle upload image

--- src/components/Customer/CardProfile.jsx
+++ src/components/Customer/CardProfile.jsx
@@ -1,15 +1,142 @@
+import { useEffect, useState, useRef } from "react";
 import { Form, Card, Row, Col, Image, Button } from "react-bootstrap";
+
+import { toast } from "react-toastify";
+import handleErrorMessage from "../../utils/handleErrorMessage";
+import { axiosInstance as axios } from "../../config/https";
+
+import { useSelector, useDispatch } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import ImgProfile from "../../assets/images/profile.png?url";
 
+const initialValues = {
+    full_name: "",
+    email: "",
+    file_name: "",
+    status: false
+};
+
+const validationSchema = Yup.object({
+    full_name: Yup.string()
+        .required("Field is required")
+        .min(4, "Input must be at least 4 characters")
+        .max(30, "Input must be at most 30 characters"),
+    email: Yup.string().required("Field is required").email(),
+    file_name: Yup.string().required("Field is required")
+});
+
 export default function CardProfile() {
+    const { user } = useSelector((state) => state.auth)
+    const _id = user._id
+    const dispatch = useDispatch()
+
+    // FORMIK
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit: handleUpdateProfile
+    });
+
+    const [isLoad, setIsLoad] = useState(true)
+    const [urlImage, setUrlImage] = useState(null)
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        if (isLoad) {
+            // SET LOADING
+            dispatch({ type: "SET_LOADING", value: true });
+            axios.get(`/users/${_id}/detail`).then((response) => {
+                const detail = response.data.data
+
+                // SET URL IMAGE
+                setUrlImage(detail.image.url)
+
+                formik.setFieldValue("full_name", detail.full_name)
+                formik.setFieldValue("email", detail.email)
+                formik.setFieldValue("status", detail.status)
+            }).catch((error) => {
+                const message = error.response?.data?.message;
+                toast(handleErrorMessage(message), {
+                    position: toast.POSITION.TOP_RIGHT,
+                    type: toast.TYPE.ERROR,
+                });
+            })
+            .finally(() => {
+                dispatch({ type: "SET_LOADING", value: false });
+                setIsLoad(false)
+            });
+        }
+    }, [isLoad, _id, dispatch, formik])
+
+    function handleUpdateProfile(form) {
+        const formData = new FormData()
+        formData.append("full_name", form.full_name)
+        formData.append("email", form.email)
+        formData.append("image", image)
+        formData.append("status", form.status)
+
+        // SET LOADING
+        dispatch({ type: "SET_LOADING", value: true });
+        axios.put(`/users/${_id}/update`, formData).then((response) => {
+            toast(response.data.message, {
+                position: toast.POSITION.TOP_RIGHT,
+                type: toast.TYPE.SUCCESS,
+            });
+        }).catch((error) => {
+            const message = error.response?.data?.message;
+            toast(handleErrorMessage(message), {
+                position: toast.POSITION.TOP_RIGHT,
+                type: toast.TYPE.ERROR,
+            });
+        })
+        .finally(() => {
+            dispatch({ type: "SET_LOADING", value: false });
+            handleResetForm()
+        });
+    }
+
+    function handleChangeImage(event) {
+        const file = event.currentTarget.files[0]
+        // set input file_name
+        formik.setFieldValue("file_name", event.currentTarget.value);
+        // set input image
+        setImage(file)
+
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            if (fileReader.readyState === 2) {
+                // set url image
+                setUrlImage(fileReader.result)
+            }
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
+    const refImage = useRef()
+    function handleResetForm() {
+        // reset input image
+        refImage.current.value = null;
+        // reset formik
+        formik.resetForm()
+        setIsLoad(true)
+    }
+
     return (
-        <Form>
+        <Form onSubmit={formik.handleSubmit}>
             <Card className="p-4 mt-md-0 mt-3">
                 <Card.Body>
                     <Row>
                         {/* IMAGE PROFILE */}
                         <Col lg="3" xs="12" className="text-center">
-                            <Image src={ImgProfile} rounded className="w-full mx-auto" />
+                            <Image 
+                                rounded
+                                src={ urlImage ? urlImage : ImgProfile} 
+                                alt="image profile"
+                                className="w-full mx-auto" 
+                                style={{ width: '100%', height: '180px', aspectRatio: '3/2', objectFit: 'contain' }}
+                            />
                         </Col>
 
                         {/* INPUT LEFT */}
@@ -23,7 +150,19 @@ export default function CardProfile() {
                                     name="full_name"
                                     type="text"
                                     placeholder="Full name"
+                                    value={formik.values.full_name}
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    className={
+                                        formik.touched.full_name && formik.errors.full_name && "border-danger"
+                                    }
                                 />
+
+                                {formik.touched.full_name && formik.errors.full_name && (
+                                    <small className="text-danger text__5">
+                                        {formik.errors.full_name}
+                                    </small>
+                                )}
                             </Form.Group>
 
                             <Form.Group className="mb-2">
@@ -31,45 +170,80 @@ export default function CardProfile() {
                                     Email
                                 </Form.Label>
                                 <Form.Control
+                                    disabled
                                     id="email"
                                     name="email"
                                     type="email"
                                     placeholder="example@gmail.com"
+                                    value={formik.values.email}
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    className={
+                                        formik.touched.email && formik.errors.email && "border-danger"
+                                    }
                                 />
+
+                                {formik.touched.email && formik.errors.email && (
+                                    <small className="text-danger text__5">
+                                        {formik.errors.email}
+                                    </small>
+                                )}
                             </Form.Group>
                         </Col>
 
                         {/* INPUT RIGHT */}
                         <Col lg="4" xs="12">
                             <Form.Group className="mb-2">
-                                <Form.Label htmlFor="image" className="mb-2">
+                                <Form.Label htmlFor="file_name" className="mb-2">
                                     Image
                                 </Form.Label>
                                 <Form.Control
-                                    id="image"
-                                    name="image"
+                                    ref={refImage}
+                                    id="file_name"
+                                    name="file_name"
                                     type="file"
                                     accept="image/*"
-                                    placeholder="Choose image"
+                                    placeholder="Choose file"
+                                    value={formik.values.file_name}
+                                    onBlur={formik.handleBlur}
+                                    onChange={handleChangeImage}
+                                    className={
+                                        formik.touched.file_name && formik.errors.file_name && "border-danger"
+                                    }
                                 />
+
+                                {formik.touched.file_name && formik.errors.file_name && (
+                                    <small className="text-danger text__5">
+                                        {formik.errors.file_name}
+                                    </small>
+                                )}
                             </Form.Group>
 
                             <Form.Group className="mb-2">
-                                <Form.Label htmlFor="password" className="mb-2">
-                                    Password
+                                <Form.Label htmlFor="status" className="mb-2">
+                                    Status
                                 </Form.Label>
-                                <Form.Control
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    placeholder="Password"
-                                />
+                                <Form.Select
+                                    disabled
+                                    id="status"
+                                    name="status"
+                                    placeholder="Status"
+                                    value={formik.values.status}
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    className={
+                                        formik.touched.status && formik.errors.status && "border-danger"
+                                    }
+                                >
+                                    <option value={false}>Non Active</option>
+                                    <option value={true}>Active</option>
+                                </Form.Select>
                             </Form.Group>
                         </Col>
 
                         {/* ACTION */}
                         <Col className="mt-2 d-flex justify-content-end">
-                            <Button type="button" variant="light" className="me-2">Cancel</Button>
+                            <Button type="button" variant="light" className="me-2" onClick={handleResetForm}>Cancel</Button>
                             <Button type="submit" variant="success">Update</Button>
                         </Col>
                     </Row>
